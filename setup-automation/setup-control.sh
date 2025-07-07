116bd02011cdcb3bdeb89a042fde053604781a89
Nuno Martins <nmartins@redhat.com>
2025-07-07 13:44:52 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -102,3 +102,165 @@ echo "Creating S3 bucket: $BUCKET_NAME in region $AWS_REGION"
 aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_REGION
 #
 #
+
+#!/bin/bash
+#
+## Fix dns resolution in automation-controller rhel9 image
+echo "search $_SANDBOX_ID.svc.cluster.local." >> /etc/resolv.conf
+echo "127.0.0.1 ansible-1 controller localhost" >> /etc/hosts
+#
+## wait for instruqt boot
+set -euxo pipefail
+while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+do
+    echo "Waiting for Instruqt to finish booting the VM"
+    sleep 1
+done
+#
+## set user name
+USER=rhel
+CONTROLLER_FQDN=controller.${_SANDBOX_ID}.svc.cluster.local
+
+## setup rhel user
+touch /etc/sudoers.d/rhel_sudoers
+echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
+cp -a /root/.ssh/* /home/$USER/.ssh/.
+chown -R rhel:rhel /home/$USER/.ssh
+
+## ansible home
+mkdir /home/$USER/ansible
+## ansible-files dir
+mkdir /home/$USER/ansible-files
+
+## ansible.cfg
+echo "[defaults]" > /home/$USER/.ansible.cfg
+echo "inventory = /home/$USER/ansible-files/hosts" >> /home/$USER/.ansible.cfg
+echo "host_key_checking = False" >> /home/$USER/.ansible.cfg
+
+## chown and chmod all files in rhel user home
+chown -R rhel:rhel /home/$USER/ansible
+chmod 777 /home/$USER/ansible
+#touch /home/rhel/ansible-files/hosts
+chown -R rhel:rhel /home/$USER/ansible-files
+
+## Set controller access env variables for system
+cat >/etc/environment <<EOL
+CONTROLLER_HOST=controller.${_SANDBOX_ID}.svc.cluster.local
+CONTROLLER_USERNAME=admin
+CONTROLLER_PASSWORD='ansible123!'
+CONTROLLER_VERIFY_SSL=false
+
+EOL
+cat /etc/environment
+
+## install python3 libraries needed for the Cloud Report
+dnf install -y python3-pip python3-libsemanage
+
+# Create a playbook for the user to execute
+tee /tmp/setup.yml << EOF
+### Automation Controller setup 
+###
+- name: Setup Controller 
+  hosts: localhost
+  connection: local
+  collections:
+    - ansible.controller
+  vars:
+    aws_access_key: "{{ lookup('env', 'AWS_ACCESS_KEY_ID') | default('AWS_ACCESS_KEY_ID_NOT_FOUND', true) }}"
+    aws_secret_key: "{{ lookup('env', 'AWS_SECRET_ACCESS_KEY') | default('AWS_SECRET_ACCESS_KEY_NOT_FOUND', true) }}"
+    aws_default_region: "{{ lookup('env', 'AWS_DEFAULT_REGION') | default('AWS_DEFAULT_REGION_NOT_FOUND', true) }}"
+  tasks:
+
+  - name: Add AWS credential
+    ansible.controller.credential:
+      name: 'AWS Credential'
+      organization: Default
+      credential_type:  "Amazon Web Services"
+      controller_host: "https://localhost"
+      controller_username: admin
+      controller_password: ansible123!
+      validate_certs: false
+      inputs:
+        username: "{{ aws_access_key }}"
+        password: "{{ aws_secret_key }}"
+
+  - name: Ensure inventory exists
+    ansible.controller.inventory:
+      controller_host: "https://localhost"
+      controller_username: admin
+      controller_password: ansible123!
+      validate_certs: false
+      name: "AWS Inventory example"
+      organization: Default
+      state: present
+    register: aws_inventory_result
+  
+  - name: Ensure AWS EC2 inventory source exists
+    ansible.controller.inventory_source:
+      controller_host: "https://localhost"
+      controller_username: admin
+      controller_password: ansible123!
+      validate_certs: false
+      name: "AWS EC2 Instances Source"
+      inventory: "AWS Inventory example"
+      source: ec2
+      credential: "AWS Credential"
+      source_vars:
+        regions: ["{{ aws_default_region }}"]
+      overwrite: true
+      overwrite_vars: true
+      update_on_launch: true
+      update_cache_timeout: 300
+      state: present
+    register: aws_inventory_source_result
+
+    
+  - name: Add EE to the controller instance
+    ansible.controller.execution_environment:
+      name: "Terraform Execution Environment"
+      image: quay.io/acme_corp/terraform_ee
+      credential: Quay Registry Credential
+      controller_username: "{{ username }}"
+      controller_password: "{{ admin_password }}"
+      controller_host: "https://{{ ansible_host }}"
+      validate_certs: false
+
+  - name: Add project
+    ansible.controller.project:
+      name: "Terraform Demos Project"
+      description: "This is from github.com/ansible-cloud"
+      organization: "Default"
+      state: present
+      scm_type: git
+      scm_url: https://github.com/HichamMourad/terraform-aap
+      default_environment: "Terraform Execution Environment"
+      controller_username: "{{ username }}"
+      controller_password: "{{ admin_password }}"
+      controller_host: "https://{{ ansible_host }}"
+      validate_certs: false
+
+  - name: Delete native job template
+    ansible.controller.job_template:
+      name: "Demo Job Template"
+      state: "absent"
+      controller_username: "{{ username }}"
+      controller_password: "{{ admin_password }}"
+      controller_host: "https://{{ ansible_host }}"
+      validate_certs: false
+
+  - name: Add a TERRAFORM INVENTORY
+    ansible.controller.inventory:
+      name: "Terraform Inventory"
+      description: "Our Terraform Inventory"
+      organization: "Default"
+      state: present
+      controller_username: "{{ username }}"
+      controller_password: "{{ admin_password }}"
+      controller_host: "https://{{ ansible_host }}"
+      validate_certs: false
+      
+EOF
+export ANSIBLE_LOCALHOST_WARNING=False
+export ANSIBLE_INVENTORY_UNPARSED_WARNING=False
+
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml
