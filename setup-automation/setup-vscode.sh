84660cfc0b6760926bc4444ef5799e016ffff640
Nuno Martins <nmartins@redhat.com>
2025-07-24 12:39:33 +0200

    Update setup-vscode.sh

--- setup-automation/setup-vscode.sh
+++ setup-automation/setup-vscode.sh
@@ -6,6 +6,12 @@ rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
 setenforce 0
 
+
+systemctl start code-server
+
+
+
+
 # cat > /tmp/requirements.yml << EOF
 # ---
 # - name: vscode-server
