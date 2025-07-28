e71935aa8d6eac6c8a703d94f161e8f35ea4914d
Nuno Martins <nmartins@redhat.com>
2025-07-28 13:11:42 +0200

    Update setup-vscode.sh

--- setup-automation/setup-vscode.sh
+++ setup-automation/setup-vscode.sh
@@ -9,8 +9,8 @@ echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
 chmod 440 /etc/sudoers.d/rhel_sudoers
 sudo -u rhel mkdir -p /home/rhel/.ssh
 sudo -u rhel chmod 700 /home/rhel/.ssh
-sudo -u rhel ssh-keygen -t ed25519 -C "rhel@$(hostname)" -f /home/rhel/.ssh/id_ed25519 -N ""
-sudo -u rhel chmod 600 /home/rhel/.ssh/id_ed25519*
+sudo -u rhel ssh-keygen -t rsa -b 4096 -C "rhel@$(hostname)" -f /home/rhel/.ssh/id_rsa -N ""
+sudo -u rhel chmod 600 /home/rhel/.ssh/id_rsa*
 
 systemctl stop firewalld
 systemctl stop code-server
